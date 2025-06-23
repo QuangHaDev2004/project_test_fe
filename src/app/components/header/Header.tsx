66bd704f882dda0da5c4170d4d9f447e1e32da4b
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-06-23 16:20:04 +0700

    add header

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+
+export const Header = () => {
+  return (
+    <>
+      <div 
+        className="bg-white py-[16px] font-2 sticky top-0 left-0 z-[997]"
+        style={{
+          boxShadow: "0px 10px 17px -9px rgba(0, 0, 0, .14)"
+        }}
+      >
+        <div className="container">
+          <div className="flex items-center justify-between">
+            {/* Logo */}
+            <Link href="/" className="font-[700] text-[40px]">
+              #
+            </Link>
+            {/* Action */}
+            <Link href="#" className="font-[700] text-[17px] hover:before:block hover:before:w-full relative before:hidden before:absolute before:top-[100%] before:w-0 before:h-[1px] before:bg-black transition duration-300 ease-in">
+              Buy now
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
